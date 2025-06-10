6c7a9b020024dddc971f697383875bd523ff8484
AlexCleberLimaFerreira <alexcleberlimaferreira@gmail.com>
2025-06-10 13:29:57 -0300

    feat:adicionando ícones

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import './Footer.css';
+
+export const Footer = () => {
+    return(
+        <footer id='footer'>
+            <div className='box-footer'>
+                <div className='logo-footer'>
+
+                    <img src={'../../../public/logo-white.svg'}/>
+                    <span> Drip Store</span>
+
+                    <p>Nos siga nas nossas redes e acompanhe nossos projetos</p>
+                    <p>balblablabla</p>
+
+                    <img src="../../../public/facebook.svg"/>
+                    <img src="../../../public/instagram.svg"/>
+                    <img src='../../../public/twitter.svg'/>
+                </div>
+            </div>
+        </footer>
+    );
+} 
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,5 +1,6 @@
 import { CardColletion } from "../components/Card/CardColletion";
 import Carousel from "../components/Carousel/Carousel";
+import { Footer } from "../components/Footer/Footer";
 import { IconColletion } from "../components/IconColletion/IconColletion";
 
 export const Inicio = () => {
@@ -8,6 +9,8 @@ export const Inicio = () => {
       <Carousel />
       <CardColletion />
       <IconColletion />
+      <Footer />
     </>
+
   );
 }
