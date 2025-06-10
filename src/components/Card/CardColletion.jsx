6a43d5faced56cead4ba718f211eb95050824bfa
Wladison-Maciel <franciscowladison97@gmail.com>
2025-06-10 09:03:18 -0300

    style | Adicionando titulo 'Coleções em destaque'

--- src/components/Card/CardColletion.jsx
+++ src/components/Card/CardColletion.jsx
@@ -3,31 +3,35 @@ import './CardColletion.css'
 
 export const CardColletion = () => {
     return (
-        <div className="card-colletion">
-            <Card
-                discount={"30% OFF"}
-                title={"Novo drop supreme"}
-                buttonText={"Teste"}
-                image={'../../src/assets/t-shirtImage.png'}
-                bgColor={'#D8E3F2'}
-                margin={'1.15rem 0 0 6.25rem'}
-            />
-            <Card
-                discount={"30% OFF"}
-                title={"Coleção Adidas"}
-                buttonText={"Teste"}
-                image={'../../src/assets/tenisImage.png'}
-                bgColor={'#D8E3F2'}
-                margin={'1.15rem 0 0 0.75rem'}
-            />
-            <Card
-                discount={"30% OFF"}
-                title={"Novo beat bass"}
-                buttonText={"Teste"}
-                image={'../../src/assets/headsetImage.png'}
-                bgColor={'#D8E3F2'}
-                margin={'1.15rem 6.25rem 0 0.75rem'}
-            />
-        </div>
+        <>
+            <div className="title-content">Coleções em destaque</div>
+            <div className="card-colletion">
+                <Card
+                    discount={"30% OFF"}
+                    title={"Novo drop supreme"}
+                    buttonText={"Teste"}
+                    image={'../../src/assets/t-shirtImage.png'}
+                    bgColor={'#D8E3F2'}
+                    margin={'1.15rem 0 0 6.25rem'}
+                />
+                <Card
+                    discount={"30% OFF"}
+                    title={"Coleção Adidas"}
+                    buttonText={"Teste"}
+                    image={'../../src/assets/tenisImage.png'}
+                    bgColor={'#D8E3F2'}
+                    margin={'1.15rem 0 0 0.75rem'}
+                />
+                <Card
+                    discount={"30% OFF"}
+                    title={"Novo beat bass"}
+                    buttonText={"Teste"}
+                    image={'../../src/assets/headsetImage.png'}
+                    bgColor={'#D8E3F2'}
+                    margin={'1.15rem 6.25rem 0 0.75rem'}
+                />
+            </div>
+        </>
+
     );
 };
