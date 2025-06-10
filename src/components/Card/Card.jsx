452cfd97cdc88eb023bca0dd009e983b98366cdd
Wladison-Maciel <franciscowladison97@gmail.com>
2025-06-10 09:37:50 -0300

    feat | Finalizando seção de Cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import './Card.css';
+import { Button } from '../Button/Button'
 
 export const Card = ({ discount, title, buttonText, image, bgColor, margin }) => {
   return (
@@ -6,7 +7,13 @@ export const Card = ({ discount, title, buttonText, image, bgColor, margin }) =>
       <div className="card-left">
         <span className="badge">{discount}</span>
         <h3 className="card-title">{title}</h3>
-        <button className="buy-btn">{buttonText}</button>
+        <Button
+        width={'9.563rem'}
+        height={'3rem'}
+        children={'Comprar'}
+        margin={'1.25rem 0 0 0'}
+        color={'#C92071'}
+        />
       </div>
       <div className="card-right">
         <img src={image} alt={title} className="card-image" />
