10b45b325926e9289931b7e08214c2b95bdc4f8d
AlexCleberLimaFerreira <alexcleberlimaferreira@gmail.com>
2025-06-11 19:00:11 -0300

    feat:ajustes no footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,18 +2,48 @@ import './Footer.css';
 
 export const Footer = () => {
     return (
-        <footer id='footer'>
-            <div className='box-footer'>
-                <div className='logo-footer'>
-                    <img src={'../../../public/logo-white.svg'} />
-                    <span> Drip Store</span>
-                    <p>Nos siga nas nossas redes e acompanhe nossos projetos</p>
-                    <p>balblablabla</p>
-                    <img src="../../../public/facebook.svg" />
-                    <img src="../../../public/instagram.svg" />
-                    <img src='../../../public/twitter.svg' />
-                </div>
+
+        <div className='box-footer'>
+            <div className='logo-footer'>
+                <img src={'../../../public/logo-white.svg'} />
+                <span> Drip Store</span>
+                <p id='teste'>Nos siga nas nossas redes e acompanhe nossos projetos</p>
+
+
+                <p id='teste'>balblablabla</p>
+                <img src="../../../public/facebook.svg" />
+                <img src="../../../public/instagram.svg" />
+                <img src='../../../public/twitter.svg' />
+
+
+            <div className='informacoes'>
+                <p id='titulo-informacao'>Contato</p>
+                <p>Segurança</p>
+                <p>Wishlist</p>
+                <p>blog</p>
+                <p>Trabalhe Conosco</p>
+                <p>Meus pedidos</p>
             </div>
-        </footer>
+
+            <div className='categorias'>
+                <p id='titulo-categoria'>Categorias</p>
+                <p>Camisetas</p>
+                <p>Calças</p>
+                <p>Bonés</p>
+                <p>Headphones</p>
+                <p>Tênis</p>
+            </div>
+
+            <div className='contatos'>
+                <p id='titulo-contato'>Contato</p>
+                <p>Av.Santos Dumond,1510-1</p>
+                <p>andar-Aldeota,Fortaleza-</p>
+                <p>CE,60150-161</p>
+                <p>(85) 3051-3411</p>
+            </div>
+            
+            </div>
+
+        </div>
     );
 } 
