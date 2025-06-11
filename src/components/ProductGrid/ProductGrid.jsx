9d8c53f84aa26692e64ff9f39cfb385f817e7a39
Wladison-Maciel <franciscowladison97@gmail.com>
2025-06-11 10:57:13 -0300

    feat | Criando grid de Produtos

---
+++ src/components/ProductGrid/ProductGrid.jsx
@@ -0,0 +1,14 @@
+import { Title } from '../Title/Title';
+import './ProductGrid.css';
+
+export const ProductGrid = () => {
+    return (
+        <div className='box-content'>
+            <div className='title-content'>
+                <Title>
+                    Produtos em destaque
+                </Title>
+            </div>
+        </div>
+    );
+};
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,6 +2,7 @@ import { CardColletion } from "../components/Card/CardColletion";
 import Carousel from "../components/Carousel/Carousel";
 import { Footer } from "../components/Footer/Footer";
 import { IconColletion } from "../components/IconColletion/IconColletion";
+import { ProductGrid } from "../components/ProductGrid/ProductGrid";
 
 export const Inicio = () => {
   return (
@@ -9,6 +10,7 @@ export const Inicio = () => {
       <Carousel />
       <CardColletion />
       <IconColletion />
+      <ProductGrid />
       <Footer />
     </>
 
