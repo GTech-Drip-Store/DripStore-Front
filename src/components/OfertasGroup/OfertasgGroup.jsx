b2661b08734733ea442876537c6451477ec5efb8
Humberto <bethosko@gmail.com>
2025-06-12 17:00:13 -0300

    feat | Adicionando seção de ofertas

---
+++ src/components/OfertasGroup/OfertasgGroup.jsx
@@ -0,0 +1,16 @@
+import './OfertasGroup.css';
+
+export const OfertasGroup = () => {
+    return (
+        <div className='box-ofertas'>
+            <div className='box-ofertas-image'>
+
+            </div>
+
+            <div className='box-ofertas-content'>
+                <p className='label-ofertas'>ofertas especiais</p>
+                <p className='c'>Air jordan edição de colecionador</p>
+            </div>
+        </div>
+    );
+};
