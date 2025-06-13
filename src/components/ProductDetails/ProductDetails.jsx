856669b8be9b0ae8ad7b8de4ff44d83d64e81cc2
Wladison-Maciel <franciscowladison97@gmail.com>
2025-06-13 16:56:37 -0300

    feat | Adicionando div para organizar exibição de produtos

---
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,22 @@
+import './ProductDetails.css'
+
+export const ProductDetails = () => {
+    return (
+        <>
+            <div className='box-caminho'>
+                <a> Home / Produtos / Tênis / Nike / Tênis Nike Revolution 6 Next Nature Masculino</a>
+            </div>
+
+
+            <div className='box-product-details'>
+                <div className='Carrosel'>
+
+                </div>
+
+                <div className='box-product-information'>
+
+                </div>
+            </div>
+        </>
+    );
+};
--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,7 +1,11 @@
+import { ProductDetails } from "../components/ProductDetails/ProductDetails";
 import { RelatedProducts } from "../components/RelatedProducts/RelatedProducts";
 
 export const Produtos = () => {
   return (
-    <RelatedProducts/>
+    <>
+      <ProductDetails />
+      <RelatedProducts />
+    </>
   );
 }
