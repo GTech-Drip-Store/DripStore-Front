ea2389135d4919f0e47dfa2745cba8bc2944e73c
Wladison-Maciel <franciscowladison97@gmail.com>
2025-06-17 15:53:52 -0300

    feat | Adicionando seleção das paginas de navegação

--- src/components/Useful/Useful.jsx
+++ src/components/Useful/Useful.jsx
@@ -1,16 +1,25 @@
-import { Link } from 'react-router-dom';
+
+import { Link, useLocation } from 'react-router-dom';
 
 export const Useful = ({ margin, color, textDecoration, to, value, target }) => {
+  const location = useLocation();
+  const isActive = location.pathname === to;
+
   return (
     <p style={{ margin: margin }}>
       <Link
         style={{
-          color: color,
-          textDecoration: textDecoration
+          color: isActive ? '#C92071' : color,
+          textDecoration: textDecoration,
+          fontWeight: isActive ? 'bold' : 'normal',
+          textDecoration: isActive ? 'underline' : 'none',
+          textUnderlineOffset: '0.125rem'
         }}
         to={to}
         target={target}
-      >{value}</Link>
+      >
+        {value}
+      </Link>
     </p>
   );
-}
+};
